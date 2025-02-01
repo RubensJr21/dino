0d2eb75c661ed9b46eb0f3fb605c40ecb77aac62
RubensJr21 <junior.correa2001@gmail.com>
2025-01-31 21:47:01 -0300

    Adiciona botões de acesso às telas de recebimento e de pagamentos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,15 +3,24 @@ import { Link } from "expo-router";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
 
-export default function Root(){
-    return (
-        <BaseView>
-            <Text variant="headlineMedium">Bem vindo ao Tabs</Text>
-            <Link href={"/expenses"} asChild>
-                <Button mode="contained">
-                    Ir para Tab
-                </Button>
-            </Link>
-        </BaseView>
-    )
+export default function Root() {
+	return (
+		<BaseView>
+			<Text variant="headlineMedium">Bem vindo ao Dino</Text>
+			<View>
+				<Link
+					href="/incomes"
+					asChild
+				>
+					<Button>Recebimentos</Button>
+				</Link>
+				<Link
+					href="/expenses"
+					asChild
+				>
+					<Button>Gastos</Button>
+				</Link>
+			</View>
+		</BaseView>
+	);
 }
