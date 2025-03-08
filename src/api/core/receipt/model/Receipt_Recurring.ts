17de757b7c3c56c1b7013dd7ca406a4bea001ec8
RubensJr21 <junior.correa2001@gmail.com>
2025-03-08 14:53:01 -0300

    Adiciona ao model Receipt_Recurring o atributo 'recurrence_type'

--- src/api/core/receipt/model/Receipt_Recurring.ts
+++ src/api/core/receipt/model/Receipt_Recurring.ts
@@ -2,4 +2,6 @@ import Receipt from "./Receipt";
 
 export default interface Receipt_Recurring extends Receipt {
 	is_disabled: boolean;
+	// fk_id_recurrence_type: IEntityBase["id"]
+	recurrence_type: string;
 }
