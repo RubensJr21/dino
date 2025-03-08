59db8a6acd6231a917bb14cc784e3c06ff6995a6
RubensJr21 <junior.correa2001@gmail.com>
2025-03-08 14:49:36 -0300

    Altera nome da variável default 'was_processed' da classe abstrata IRepository_Base_ItemValue

--- src/api/core/_shared/ports/IRepository_Base_ItemValue.ts
+++ src/api/core/_shared/ports/IRepository_Base_ItemValue.ts
@@ -3,7 +3,7 @@ import IEntityBase from "@api/core/_shared/model/IEntityBase";
 export default abstract class IRepository_Base_ItemValue<
 	T extends IEntityBase
 > {
-	protected was_processed: boolean = false;
+	protected default_was_processed: boolean = false;
 	abstract mark_receipt_as_processed(id: T["id"]): Promise<T | undefined>;
 	constructor() {}
 }
