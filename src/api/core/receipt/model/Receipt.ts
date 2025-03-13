2867744482038ace5fdccffe2de2f20fcf8ad32b
RubensJr21 <junior.correa2001@gmail.com>
2025-03-12 21:24:07 -0300

    Atualiza caminho da importação do Base_ItemValue de "@/api/core/_shared/model/Base_ItemValue" para "@core/_shared/model/Base_ItemValue"

--- src/api/core/receipt/model/Receipt.ts
+++ src/api/core/receipt/model/Receipt.ts
@@ -1,3 +1,3 @@
-import Base_ItemValue from "@/api/core/_shared/model/Base_ItemValue";
+import Base_ItemValue from "@core/_shared/model/Base_ItemValue";
 
 export default interface Receipt extends Base_ItemValue {}
