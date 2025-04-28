d8a94451e0c98ce3fcbb04d3ced93a01277f976c
RubensJr21 <junior.correa2001@gmail.com>
2025-04-28 13:20:14 -0300

    Altera dependência na interface IBaseItemValue_BankAccountTransferMethod

--- src/core/entities/base_item_value-bank_account_transfer_method.entity.ts
+++ src/core/entities/base_item_value-bank_account_transfer_method.entity.ts
@@ -1,8 +1,8 @@
-import { IBankAccountTransferMethod } from "./bank_account_transfer_method.entity";
+import { BankAccountTransferMethod } from "./bank_account_transfer_method.entity";
 import { ABaseItemValue } from "./base_item_value.entity";
 
 export interface IBaseItemValue_BankAccountTransferMethod<T extends ABaseItemValue> {
   readonly id: number;
   base_item_value: T;
-  bank_account_transfer_method: IBankAccountTransferMethod;
+  bank_account_transfer_method: BankAccountTransferMethod;
 }
