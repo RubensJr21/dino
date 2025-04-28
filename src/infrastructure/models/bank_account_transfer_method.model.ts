b4ae1b09942e98b9539bb978e13662bccefcd84a
RubensJr21 <junior.correa2001@gmail.com>
2025-04-28 12:46:26 -0300

    Corrige dependência. Faz com que a dependência seja em relação ao model criado implementando a interface, ao invés da interface em sí.

--- src/infrastructure/models/bank_account_transfer_method.model.ts
+++ src/infrastructure/models/bank_account_transfer_method.model.ts
@@ -1,5 +1,6 @@
 import { IBankAccountTransferMethod } from "@src/core/entities/bank_account_transfer_method.entity";
+import { MBankAccount } from "./bank_account.model";
 
 export interface MBankAccountTransferMethod extends StrictOmit<IBankAccountTransferMethod, "bank_account"> {
-  bank_account_id: IBankAccountTransferMethod["bank_account"]["id"]
+  bank_account_id: MBankAccount["id"]
 }
