cb4d20f71319c4590b71d7313898a946ee29476b
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:44:44 -0300

    Atualiza credit-card/lista_all para usar os Value Objects

--- src/core/use_cases/credit-card/list_all.use_case.ts
+++ src/core/use_cases/credit-card/list_all.use_case.ts
@@ -1,12 +1,14 @@
 import { CreditCard } from "@core/entities/credit_card.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoCreditCard } from "@core/shared/RepositoryTypes";
+import { IRepoCreditCard } from "@src/infrastructure/repositories/drizzle/credit_card.repository";
 
 export default class ListAllCreditCards implements IUseCase<void, CreditCard[]>{
-    constructor(
-        private repo_cc: IRepoCreditCard
-    ){}
-    async execute(): Promise<CreditCard[]> {
-        return await this.repo_cc.findAll()
-    }
+  constructor(
+    private repo_cc: IRepoCreditCard
+  ){}
+  async execute(): Promise<CreditCard[]> {
+    const credit_cards_models = await this.repo_cc.findAll()
+    const credit_cards = credit_cards_models.map(cc_ac => new CreditCard(cc_ac))
+    return credit_cards;
+  }
 }
