099beaf757bd4d900d42f3cb503a5aef40d2dd36
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:06:49 -0300

    Atualiza bank-account/disable para usar os Value Objects

--- src/core/use_cases/bank-account/disable.use_case.ts
+++ src/core/use_cases/bank-account/disable.use_case.ts
@@ -1,33 +1,32 @@
 import { BankAccount } from "@core/entities/bank_account.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoBankAccount } from "@core/shared/RepositoryTypes";
+import { IRepoBankAccount } from "@src/infrastructure/repositories/drizzle/bank_account.repository";
 
 interface DisableBankAccount_Input {
-    id: number,
+  id: number,
 }
 
 export default class DisableBankAccount implements IUseCase<DisableBankAccount_Input, BankAccount> {
-    constructor(
-        private repo_ba: IRepoBankAccount
-    ){}
-    async execute(input: DisableBankAccount_Input): Promise<BankAccount> {
-        const bank_account = await this.repo_ba.findById(input.id)
+  constructor(
+    private repo_ba: IRepoBankAccount
+  ){}
+  async execute(input: DisableBankAccount_Input): Promise<BankAccount> {
+    const bank_account_model = await this.repo_ba.findById(input.id)
 
-        if (!bank_account){
-            throw new Error("Invalid id bank account!")
-        }
-        
-        if (bank_account.is_disabled === true){
-            throw new Error("Bank account is already disable!")
-        }
-
-        const bank_account_updated = await this.repo_ba.update({
-            ...bank_account,
-            is_disabled: true
-        })
-
-        if(!bank_account_updated) throw new Error("Invalid Bank Account returned!")
-
-        return bank_account_updated;
+    if (!bank_account_model){
+      throw new Error("Invalid id bank account!")
     }
+
+    const bank_account = new BankAccount(bank_account_model)
+
+    bank_account.disable()
+
+    const {id, ...bank_account_without_id} = bank_account.properties
+
+    const bank_account_updated = await this.repo_ba.update(id, bank_account_without_id)
+
+    if(!bank_account_updated) throw new Error("An error occurred while updating the bank account.")
+
+    return bank_account;
+  }
 }
