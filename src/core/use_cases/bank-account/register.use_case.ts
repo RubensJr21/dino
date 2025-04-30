faff72eb5b0a6036bd5d986a2c50f88e8faf464c
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:28:29 -0300

    Atualiza bank-account/register para usar os Value Objects

--- src/core/use_cases/bank-account/register.use_case.ts
+++ src/core/use_cases/bank-account/register.use_case.ts
@@ -1,30 +1,36 @@
-import { BankAccount } from "@core/entities/bank_account.entity";
-import { DTO_BankAccount } from "@core/shared/DTOTypes";
+import { BankAccount, IBankAccount } from "@core/entities/bank_account.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoBankAccount } from "@core/shared/RepositoryTypes";
+import { IRepoBankAccount } from "@src/infrastructure/repositories/drizzle/bank_account.repository";
 
-type BankAccount_RegisterInput = StrictOmit<DTO_BankAccount, "is_disabled">
+type BankAccount_RegisterInput = Pick<IBankAccount, "nickname"|"balance">
 
 export default class RegisterBankAccount implements IUseCase<BankAccount_RegisterInput, BankAccount> {
-    constructor(
-        private repo_ba: IRepoBankAccount
-    ){}
-    async execute(input: BankAccount_RegisterInput): Promise<BankAccount> {
-        // REVIEW: Quando um banco é adicionado é necessário popular a tabela de bank_account_transfer_method
+  constructor(
+    private repo_ba: IRepoBankAccount
+  ){}
+  async execute(input: BankAccount_RegisterInput): Promise<BankAccount> {
+    // REVIEW: Quando um banco é adicionado é necessário popular a tabela de bank_account_transfer_method
 
-        const ba_nickname = await this.repo_ba.findByNickname(input.nickname)
-        if(ba_nickname){
-            throw new Error("Nickname already used by another bank account!")
-        }
-
-        const bank_account = await this.repo_ba.create({
-            ...input,
-            is_disabled: false
-        })
-
-        if(!bank_account) throw new Error("Invalid Bank Account returned!")
-
-        return bank_account;
+    const nickname = await this.repo_ba.findByNickname(input.nickname)
+    if(nickname){
+      throw new Error("Nickname already used by another bank account!")
     }
 
+    const current_date = new Date()
+
+    const bank_account_data = {
+      ...input,
+      is_disabled: false,
+      created_at: current_date,
+      updated_at: current_date
+    }
+
+    const bank_account_created = await this.repo_ba.create(bank_account_data)
+
+    if(!bank_account_created) {
+      throw new Error("An error occurred while creating the bank account.")
+    }
+    
+    return new BankAccount(bank_account_created);
+  }
 }
