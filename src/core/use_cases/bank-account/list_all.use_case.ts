3086bdaa06f5175a33b26515b170b95fb175632b
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:14:56 -0300

    Atualiza bank-account/list_all para usar os Value Objects

--- src/core/use_cases/bank-account/list_all.use_case.ts
+++ src/core/use_cases/bank-account/list_all.use_case.ts
@@ -1,13 +1,14 @@
 import { BankAccount } from "@core/entities/bank_account.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoBankAccount } from "@core/shared/RepositoryTypes";
+import { IRepoBankAccount } from "@infrastructure/repositories/drizzle/bank_account.repository";
 
 export default class ListAllBankAccounts implements IUseCase<void, BankAccount[]> {
-    constructor(
-        private repo_ba: IRepoBankAccount
-    ){}
-    async execute(): Promise<BankAccount[]> {
-        return await this.repo_ba.findAll();
-    }
-
+  constructor(
+    private repo_ba: IRepoBankAccount
+  ){}
+  async execute(): Promise<BankAccount[]> {
+    const banks_account_models = await this.repo_ba.findAll()
+    const banks_account = banks_account_models.map(bk_ac => new BankAccount(bk_ac))
+    return banks_account;
+  }
 }
