e9c205e8fd3ea00c87773a90fcc0adf0c65c51ef
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 22:07:18 -0300

    Adiciona comentário ao credit_card.entity:enable() para validação futura

--- src/core/entities/credit_card.entity.ts
+++ src/core/entities/credit_card.entity.ts
@@ -85,6 +85,12 @@ export class CreditCard implements ICreditCard {
     return this._is_disabled
   }
   public enabled(): void {
+    // TODO: Será retornado um erro para que
+    // não seja necessário enviar os dados para o banco de dados
+    // sem uma mudança efetiva 
+    // if (credit_card.is_disabled === false){
+    //   throw new Error("Credit card is already enable!")
+    // }
     this._is_disabled = false;
   }
   public disable(): void {
