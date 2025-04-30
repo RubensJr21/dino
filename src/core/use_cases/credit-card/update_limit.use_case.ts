542dbc6dd6815d4fa74dfbe461a6c746fb7a178d
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:53:10 -0300

    Atualiza credit-card/update_limit para usar os Value Objects

--- src/core/use_cases/credit-card/update_limit.use_case.ts
+++ src/core/use_cases/credit-card/update_limit.use_case.ts
@@ -1,31 +1,31 @@
 import { CreditCard } from "@core/entities/credit_card.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoCreditCard } from "@core/shared/RepositoryTypes";
+import { IRepoCreditCard } from "@src/infrastructure/repositories/drizzle/credit_card.repository";
 
 interface UpdateLimitCreditCard_Input {
-    id: number,
-    new_limit: number
+  id: number,
+  new_limit: number
 }
 
 export default class UpdateLimitCreditCard implements IUseCase<UpdateLimitCreditCard_Input, CreditCard>{
-    constructor(
-        private repo_cc: IRepoCreditCard
-    ){}
-    async execute(input: UpdateLimitCreditCard_Input): Promise<CreditCard> {
-        const credit_card = await this.repo_cc.findById(input.id)
-
-        if (!credit_card){
-            throw new Error("Invalid id credit card")
-        }
-
-        const credit_card_updated = await this.repo_cc.update({
-            ...credit_card,
-            limit: input.new_limit
-        })
-
-        if(!credit_card_updated) throw new Error("Invalid Credit Card returned!")
-
-        return credit_card_updated
+  constructor(
+    private repo_cc: IRepoCreditCard
+  ){}
+  async execute(input: UpdateLimitCreditCard_Input): Promise<CreditCard> {
+    const credit_card_model = await this.repo_cc.findById(input.id)
+    if (!credit_card_model){
+      throw new Error("Invalid id credit card")
     }
 
+    const credit_card = new CreditCard(credit_card_model)
+    credit_card.change_limit(input.new_limit)
+
+    const {id, ...credit_card_without_id} = credit_card.properties
+
+    const credit_card_updated = await this.repo_cc.update(id, credit_card_without_id)
+
+    if(!credit_card_updated) throw new Error("Invalid Credit Card returned!")
+
+    return credit_card
+  }
 }
