68789baf869e25ac1dff13283addaada532bd424
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:43:04 -0300

    Atualiza credit-card/enable para usar os Value Objects

--- src/core/use_cases/credit-card/enable.use_case.ts
+++ src/core/use_cases/credit-card/enable.use_case.ts
@@ -1,34 +1,30 @@
 import { CreditCard } from "@core/entities/credit_card.entity";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoCreditCard } from "@core/shared/RepositoryTypes";
+import { IRepoCreditCard } from "@src/infrastructure/repositories/drizzle/credit_card.repository";
 
 interface EnableCreditCard_Input {
-    id: number
+  id: number
 }
 
 export default class EnableCreditCard implements IUseCase<EnableCreditCard_Input, CreditCard>{
-    constructor(
-        private repo_cc: IRepoCreditCard
-    ){}
-    async execute(input: EnableCreditCard_Input): Promise<CreditCard> {
-        const credit_card = await this.repo_cc.findById(input.id)
-
-        if (!credit_card){
-            throw new Error("Invalid id credit card")
-        }
-
-        if (credit_card.is_disabled === false){
-            throw new Error("Credit card is already enable!")
-        }
-
-        const credit_card_updated = await this.repo_cc.update({
-            ...credit_card,
-            is_disabled: false
-        })
-
-        if(!credit_card_updated) throw new Error("Invalid Credit Card returned!")
-
-        return credit_card_updated
+  constructor(
+    private repo_cc: IRepoCreditCard
+  ){}
+  async execute(input: EnableCreditCard_Input): Promise<CreditCard> {
+    const credit_card_model = await this.repo_cc.findById(input.id)
+    if (!credit_card_model){
+      throw new Error("Invalid id credit card")
     }
 
+    const credit_card = new CreditCard(credit_card_model)
+    credit_card.enabled()
+    const {id, ...credit_card_without_id} = credit_card.properties
+
+    const credit_card_updated = await this.repo_cc.update(id, credit_card_without_id)
+    if(!credit_card_updated) {
+      throw new Error("An error occurred while updating the credit card.")
+    }
+
+    return credit_card
+  }
 }
