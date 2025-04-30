e7b507f232b11c123fa7e4d483edb75f223befba
RubensJr21 <junior.correa2001@gmail.com>
2025-04-29 21:48:53 -0300

    Atualiza credit-card/register para usar os Value Objects

--- src/core/use_cases/credit-card/register.use_case.ts
+++ src/core/use_cases/credit-card/register.use_case.ts
@@ -1,28 +1,33 @@
 import { CreditCard } from "@core/entities/credit_card.entity";
-import { DTO_CreditCard } from "@core/shared/DTOTypes";
 import IUseCase from "@core/shared/IUseCase";
-import { IRepoCreditCard } from "@core/shared/RepositoryTypes";
+import { IRepoCreditCard } from "@src/infrastructure/repositories/drizzle/credit_card.repository";
 
-type CreditCard_RegisterInput = StrictOmit<DTO_CreditCard, "is_disabled">
+type CreditCard_RegisterInput = StrictOmit<CreditCard, "is_disabled"|"created_at"|"updated_at">
 
 export default class RegisterCreditCard implements IUseCase<CreditCard_RegisterInput, CreditCard>{
-    constructor(
-        private repo_cc: IRepoCreditCard
-    ){}
-    async execute(input: CreditCard_RegisterInput): Promise<CreditCard> {
-        const cc_nickname = await this.repo_cc.findByNickname(input.nickname)
-        if(cc_nickname){
-            throw new Error("Nickname already used by another credit card!")
-        }
-
-        const credit_card = await this.repo_cc.create({
-            ...input,
-            is_disabled: false
-        })
-
-        if(!credit_card) throw new Error("Invalid Credit Card returned!")
-
-        return credit_card
+  constructor(
+    private repo_cc: IRepoCreditCard
+  ){}
+  async execute(input: CreditCard_RegisterInput): Promise<CreditCard> {
+    const cc_nickname = await this.repo_cc.findByNickname(input.nickname)
+    if(cc_nickname){
+      throw new Error("Nickname already used by another credit card!")
     }
 
+    const current_date = new Date()
+
+    const credit_card_data = {
+      ...input,
+      is_disabled: false,
+      created_at: current_date,
+      updated_at: current_date
+    }
+
+    const credit_card_created = await this.repo_cc.create(credit_card_data)
+
+    if(!credit_card_created) {
+      throw new Error("An error occurred while creating the bank account.")
+    }
+    return new CreditCard(credit_card_created)
+  }
 }
