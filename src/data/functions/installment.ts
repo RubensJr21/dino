882fb9164e60f4f3b36402591fbd2738a56b8a22
RubensJr21 <junior.correa2001@gmail.com>
2025-09-25 14:55:58 -0300

    adiciona as modificações de estratégias para installment

--- src/data/functions/installment.ts
+++ src/data/functions/installment.ts
@@ -1,170 +1,183 @@
 import { type DatabaseType } from "@database/db-instance";
 import {
+  bankAccount,
   baseTransactionType,
+  category,
   installment,
   installmentItemValue,
   itemValue,
   transactionInstrument,
   transferMethod,
 } from "@database/schema";
-import { and, eq } from "drizzle-orm";
+import { and, eq, sql } from "drizzle-orm";
 
 type DataInsert = typeof installment.$inferInsert;
 type DataSelect = typeof installment.$inferSelect;
 
 export async function get(
-	db: DatabaseType,
-	installment_id: typeof installment.$inferSelect.id
+  db: DatabaseType,
+  installment_id: typeof installment.$inferSelect.id
 ) {
-	return (
-		await db
-			.select({
-				id: installment.id,
+  return (
+    await db
+      .select({
+        id: installment.id,
 
-				cashflow_type: baseTransactionType.cashflow_type,
+        cashflow_type: baseTransactionType.cashflow_type,
+        description: baseTransactionType.description,
 
-				start_date: installment.start_date,
-				installment_numbers: installment.installments_number,
-				total_amount: installment.total_amount,
+        category_id: baseTransactionType.fk_id_category,
+        category_code: category.code,
 
-				transaction_instrument_id: transactionInstrument.id,
-				bank_account_id: transactionInstrument.fk_id_bank_account,
+        transaction_instrument_id: baseTransactionType.fk_id_transaction_instrument,
+        transaction_instrument_nickname: sql<string>`
+          CASE
+            WHEN ${bankAccount.id} IS NULL
+            THEN ${transferMethod.code}
+            ELSE ${transferMethod.code} || ' - ' || ${bankAccount.nickname}
+          END
+        `.as("transaction_instrument_nickname"),
+        bank_account_id: transactionInstrument.fk_id_bank_account,
 
-				transfer_method_code: transferMethod.code,
-			})
-			.from(installment)
-			.innerJoin(
-				baseTransactionType,
-				eq(installment.id, baseTransactionType.id)
-			)
-			.innerJoin(
-				transactionInstrument,
-				eq(
-					baseTransactionType.fk_id_transaction_instrument,
-					transactionInstrument.id
-				)
-			)
-			.innerJoin(
-				transferMethod,
-				eq(transactionInstrument.fk_id_transfer_method, transferMethod.id)
-			)
-			.where(eq(installment.id, installment_id))
-	).shift();
+        transfer_method_code: transferMethod.code,
+
+        start_date: installment.start_date,
+        installments_number: installment.installments_number,
+        total_amount: installment.total_amount,
+      })
+      .from(installment)
+      .innerJoin(
+        baseTransactionType,
+        eq(installment.id, baseTransactionType.id)
+      )
+      .innerJoin(
+        transactionInstrument,
+        eq(
+          baseTransactionType.fk_id_transaction_instrument,
+          transactionInstrument.id
+        )
+      )
+      .innerJoin(
+        transferMethod,
+        eq(transactionInstrument.fk_id_transfer_method, transferMethod.id)
+      )
+      .where(eq(installment.id, installment_id))
+  ).shift();
 }
 
 export const calculate_installments = (
-	total_amount: DataSelect["total_amount"],
-	installments_number: DataSelect["total_amount"]
+  total_amount: DataSelect["total_amount"],
+  installments_number: DataSelect["total_amount"]
 ): [first_installment: number, others: number] => {
-	const installment_value = Math.trunc(total_amount / installments_number);
-	const residual_installment_value = total_amount % installments_number;
+  const installment_value = Math.trunc(total_amount / installments_number);
+  const residual_installment_value = total_amount % installments_number;
 
-	return [installment_value + residual_installment_value, installment_value];
+  return [installment_value + residual_installment_value, installment_value];
 };
 
 export async function insert(db: DatabaseType, data: DataInsert) {
-	return await db.insert(installment).values(data).returning();
+  return await db.insert(installment).values(data).returning();
 }
 
 export async function register_item_value(
-	db: DatabaseType,
-	data: typeof installmentItemValue.$inferInsert
+  db: DatabaseType,
+  data: typeof installmentItemValue.$inferInsert
 ) {
-	return await db.insert(installmentItemValue).values(data).returning();
+  return await db.insert(installmentItemValue).values(data).returning();
 }
 export async function get_all_item_values(
-	db: DatabaseType,
-	installment_id: typeof installment.$inferSelect.id
+  db: DatabaseType,
+  installment_id: typeof installment.$inferSelect.id
 ) {
-	return await db
-		.select({
-			id: itemValue.id,
-			scheduled_at: itemValue.scheduled_at,
-			amount: itemValue.amount,
-			was_processed: itemValue.was_processed,
+  return await db
+    .select({
+      id: itemValue.id,
+      scheduled_at: itemValue.scheduled_at,
+      amount: itemValue.amount,
+      was_processed: itemValue.was_processed,
 
-			cashflow_type: baseTransactionType.cashflow_type,
+      cashflow_type: baseTransactionType.cashflow_type,
 
-			bank_account_id: transactionInstrument.fk_id_bank_account,
+      bank_account_id: transactionInstrument.fk_id_bank_account,
 
-			transfer_method_code: transferMethod.code,
-		})
-		.from(installmentItemValue)
-		.where(eq(installmentItemValue.fk_id_installment, installment_id))
-		.innerJoin(
-			itemValue,
-			eq(installmentItemValue.fk_id_item_value, itemValue.id)
-		)
-		.innerJoin(
-			baseTransactionType,
-			eq(installmentItemValue.fk_id_installment, baseTransactionType.id)
-		)
-		.innerJoin(
-			transactionInstrument,
-			eq(
-				transactionInstrument.id,
-				baseTransactionType.fk_id_transaction_instrument
-			)
-		)
-		.innerJoin(
-			transferMethod,
-			eq(transferMethod.id, transactionInstrument.fk_id_transfer_method)
-		);
+      transfer_method_code: transferMethod.code,
+    })
+    .from(installmentItemValue)
+    .where(eq(installmentItemValue.fk_id_installment, installment_id))
+    .innerJoin(
+      itemValue,
+      eq(installmentItemValue.fk_id_item_value, itemValue.id)
+    )
+    .innerJoin(
+      baseTransactionType,
+      eq(installmentItemValue.fk_id_installment, baseTransactionType.id)
+    )
+    .innerJoin(
+      transactionInstrument,
+      eq(
+        transactionInstrument.id,
+        baseTransactionType.fk_id_transaction_instrument
+      )
+    )
+    .innerJoin(
+      transferMethod,
+      eq(transferMethod.id, transactionInstrument.fk_id_transfer_method)
+    );
 }
 
 export async function get_item_value(
-	db: DatabaseType,
-	installment_id: typeof installment.$inferSelect.id,
-	item_value_id: typeof itemValue.$inferSelect.id
+  db: DatabaseType,
+  installment_id: typeof installment.$inferSelect.id,
+  item_value_id: typeof itemValue.$inferSelect.id
 ) {
-	return (
-		await db
-			.select({
-				id: itemValue.id,
-				scheduled_at: itemValue.scheduled_at,
-				amount: itemValue.amount,
-				was_processed: itemValue.was_processed,
+  return (
+    await db
+      .select({
+        id: itemValue.id,
+        scheduled_at: itemValue.scheduled_at,
+        amount: itemValue.amount,
+        was_processed: itemValue.was_processed,
 
-				cashflow_type: baseTransactionType.cashflow_type,
+        cashflow_type: baseTransactionType.cashflow_type,
 
-				bank_account_id: transactionInstrument.fk_id_bank_account,
+        bank_account_id: transactionInstrument.fk_id_bank_account,
 
-				transfer_method_code: transferMethod.code,
-			})
-			.from(installmentItemValue)
-			.where(
-				and(
-					eq(installmentItemValue.fk_id_installment, installment_id),
-					eq(itemValue.id, item_value_id)
-				)
-			)
-			.innerJoin(
-				itemValue,
-				eq(installmentItemValue.fk_id_item_value, itemValue.id)
-			)
-			.innerJoin(
-				baseTransactionType,
-				eq(installmentItemValue.fk_id_installment, baseTransactionType.id)
-			)
-			.innerJoin(
-				transactionInstrument,
-				eq(
-					transactionInstrument.id,
-					baseTransactionType.fk_id_transaction_instrument
-				)
-			)
-			.innerJoin(
-				transferMethod,
-				eq(transferMethod.id, transactionInstrument.fk_id_transfer_method)
-			)
-	).shift();
+        transfer_method_code: transferMethod.code,
+      })
+      .from(installmentItemValue)
+      .where(
+        and(
+          eq(installmentItemValue.fk_id_installment, installment_id),
+          eq(itemValue.id, item_value_id)
+        )
+      )
+      .innerJoin(
+        itemValue,
+        eq(installmentItemValue.fk_id_item_value, itemValue.id)
+      )
+      .innerJoin(
+        baseTransactionType,
+        eq(installmentItemValue.fk_id_installment, baseTransactionType.id)
+      )
+      .innerJoin(
+        transactionInstrument,
+        eq(
+          transactionInstrument.id,
+          baseTransactionType.fk_id_transaction_instrument
+        )
+      )
+      .innerJoin(
+        transferMethod,
+        eq(transferMethod.id, transactionInstrument.fk_id_transfer_method)
+      )
+  ).shift();
 }
 
 export async function remove(
-	db: DatabaseType,
-	installment_id: typeof installment.$inferSelect.id
+  db: DatabaseType,
+  installment_id: typeof installment.$inferSelect.id
 ) {
-	await db.delete(installment).where(eq(installment.id, installment_id));
+  await db.delete(installment).where(eq(installment.id, installment_id));
 }
 
 export type { DataInsert as infer_insert, DataSelect as infer_select };
--- src/lib/strategies/installment.ts
+++ src/lib/strategies/installment.ts
@@ -1,6 +1,40 @@
-import InstallmentPaymentApi from "data/api/payment/installment.api"
-import InstallmentReceiptApi from "data/api/receipt/installment.api"
-import { InstallmentScreenInsert, Kind } from "../types"
+import { find_installment } from "@data/playground/installment/find";
+import { insert_installment } from "@data/playground/installment/insert";
+import { getCashflowType, InstallmentScreenInsert, Kind } from "../types";
+
+export async function sharedInsert(data: InstallmentScreenInsert, kind: Kind) {
+  return await insert_installment({
+    description: data.description,
+    cashflow_type: getCashflowType(kind),
+    category_id: data.category.id,
+    transaction_instrument_id: data.transactionInstrument.id,
+    transfer_method_code: data.transactionInstrument.transfer_method_code,
+    start_date: data.startDate,
+    installments_number: Number(data.installments),
+    total_amount: Number(data.amountValue)
+  })
+}
+export async function sharedFetch(id: string): Promise<InstallmentScreenInsert | undefined> {
+  const installment_founded = await find_installment(Number(id))
+  if (installment_founded === undefined) {
+    return undefined;
+  }
+  return {
+    description: installment_founded.description,
+    category: {
+      id: installment_founded.category_id,
+      code: installment_founded.category_code
+    },
+    transactionInstrument: {
+      id: installment_founded.transaction_instrument_id,
+      nickname: installment_founded.transaction_instrument_nickname,
+      transfer_method_code: installment_founded.transfer_method_code
+    },
+    amountValue: installment_founded.total_amount.toFixed(2),
+    startDate: installment_founded.start_date,
+    installments: installment_founded.installments_number.toFixed(0)
+  }
+}
 
 // Parcelados
 export const installmentStrategies: Record<
@@ -11,35 +45,11 @@ export const installmentStrategies: Record<
   }
 > = {
   payment: {
-    insert: async (data) => {
-      const result = await InstallmentPaymentApi.register({
-        description: data.description,
-        startDate: data.startDate,
-        installments: Number(data.installments),
-        totalAmount: Number(data.amountValue),
-        tagSelected: data.tagSelected,
-        transferMethodId: data.transferMethodSelected.id
-      })
-      console.log(result?.properties)
-    },
-    fetchById: async (id) => {
-      return await InstallmentPaymentApi.find_by_id({ id: Number(id) })
-    }
+    insert: async (data) => sharedInsert(data, "payment"),
+    fetchById: async (id) => sharedFetch(id)
   },
   receipt: {
-    insert: async (data) => {
-      const result = await InstallmentReceiptApi.register({
-        description: data.description,
-        startDate: data.startDate,
-        installments: Number(data.installments),
-        totalAmount: Number(data.amountValue),
-        tagSelected: data.tagSelected,
-        transferMethodId: data.transferMethodSelected.id
-      })
-      console.log(result?.properties)
-    },
-    fetchById: async (id) => {
-      return await InstallmentReceiptApi.find_by_id({ id: Number(id) })
-    }
+    insert: async (data) => sharedInsert(data, "receipt"),
+    fetchById: async (id) => sharedFetch(id)
   }
 }
